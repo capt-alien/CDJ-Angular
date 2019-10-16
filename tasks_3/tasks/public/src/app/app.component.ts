74c98b3dc98f94975dbdba4bd4b22d0365f6d3c9
capt-alien <eric.botcher@presidio.edu>
2019-10-16 14:50:38 -0700

    no ignore

---
+++ tasks_3/tasks/public/src/app/app.component.ts
@@ -0,0 +1,70 @@
+import { Component,OnInit } from '@angular/core';
+import { HttpService } from './http.service';
+import { observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+
+export class AppComponent implements OnInit {
+  newTask: any;
+  task: any;
+  tasks = [];
+  errors: any = [];
+
+
+  constructor(private _httpService:HttpService){}
+
+  ngOnInit(): void {
+    // This is where new tasks is instantied
+    this.newTask = { title: "",
+                     desc: "" }
+  }
+  // READ
+  showAllTasksOnClick(){
+    let observable = this._httpService.getTasks();
+    observable.subscribe((data:any) => {
+       console.log("Got our tasks!", data)
+       this.tasks = data;
+    });
+  }
+  // READ ONE
+  showTaskDetails(id){
+    console.log(id)
+    const observable = this._httpService.getOnebyID(id);
+    console.log(observable)
+    observable.subscribe((data:any)=>{
+      console.log("got task", data);
+      this.task = data
+    })
+  }
+  // DELETE
+  deleteTask(id){
+    console.log(id)
+    const observable = this._httpService.deleteOneById(id);
+    console.log(id+" deleted");
+    observable.subscribe((data: any)=>{
+      this.showAllTasksOnClick();
+    })
+
+  }
+  // CREATE
+  onSubmit(){
+    // debugger;
+    const observable = this._httpService.createNew(this.newTask);
+    observable.subscribe((data: any) => {
+      // tslint:disable-next-line:no-debugger
+      // debugger;
+      if (data.message === 'fail') {
+        this.errors = data.errors;
+        console.log(this.errors);
+      };
+    this.newTask = { title: "",
+                     desc: "" ,}
+  });
+  };
+};
